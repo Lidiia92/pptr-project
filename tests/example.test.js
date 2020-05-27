be384ab543282b26f71ddfe7020e481699743be3
Lidii <lidiiany92@gmail.com>
2020-05-27 16:39:04 -0700

    interacting with dropdown

--- tests/example.test.js
+++ tests/example.test.js
@@ -2,11 +2,24 @@ const puppeteer = require('puppeteer');
 
 describe('First Test', () => {
 	it('should launch the browser', async function () {
-		const browser = await puppeteer.launch({ headless: false });
+		const browser = await puppeteer.launch({
+			headless: false,
+			slowMo: 500,
+			devtools: false,
+		});
 		const page = await browser.newPage();
 
-		await page.goto('http://example.com');
-		await page.waitForSelector('h1');
+		// await page.goto('http://example.com');
+		// await page.waitFor(3000);
+		// await page.waitForSelector('h1');
+
+		await page.goto('https://devexpress.github.io/testcafe/example');
+		await page.type('#developer-name', 'Mike');
+		await page.click('#tried-test-cafe', { clickCount: 1 });
+		await page.select('#preferred-interface', 'JavaScript API');
+		await page.type('#comments', 'Test message');
+		await page.click('#submit-message');
+		await page.waitForSelector('.result-content');
 		await browser.close();
 	});
 });
