6661c8838ee3f98dd3b5cc7afd581c760da8b294
Lidii <lidiiany92@gmail.com>
2020-05-27 16:59:01 -0700

    Added assertions

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const expect = require('chai').expect;
 
 describe('First Test', () => {
 	it('should launch the browser', async function () {
@@ -10,8 +11,6 @@ describe('First Test', () => {
 		const page = await browser.newPage();
 
 		// await page.goto('http://example.com');
-		// await page.waitFor(3000);
-		// await page.waitForSelector('h1');
 
 		await page.goto('https://devexpress.github.io/testcafe/example');
 		await page.type('#developer-name', 'Mike');
@@ -20,6 +19,20 @@ describe('First Test', () => {
 		await page.type('#comments', 'Test message');
 		await page.click('#submit-message');
 		await page.waitForSelector('.result-content');
+
+		// Getting title, url and element text
+		const title = await page.title;
+		const url = await page.url();
+		const text = await page.$eval('h1', (element) => element.textContent);
+
+		//Get element count
+		const count = await page.$$eval('p', (element) => element.length);
+
+		//Assertions
+		expect(title).to.be.a('string', 'Example Domain');
+		expect(url).to.include('example.com');
+		expect(count).to.equal(2);
+
 		await browser.close();
 	});
 });
